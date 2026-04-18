bfeb8c6f27f63ac86df5ae5d58e40312795ce5d1
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2026-04-19 09:24:39 +1200

    add jellyfin stats source

--- src/main/stats/stats.manager.ts
+++ src/main/stats/stats.manager.ts
@@ -1,8 +1,10 @@
+import { JellyfinStatsSource } from "./source/jellyfin.stats-source";
 import { SensorsStatsSource } from "./source/sensors.stats-source";
 import { SlskdStatsSource } from "./source/slskd.stats-source";
 import { StatsSource } from "./source/stats-source";
 
 export const STATS_SOURCES: StatsSource[] = [
 	new SlskdStatsSource(),
+	new JellyfinStatsSource(),
 	new SensorsStatsSource(),
 ] as const;
